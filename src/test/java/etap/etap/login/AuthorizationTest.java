6f8774c736d8362e4d2380909c63241373916d63
rakeshine <44422808+rakeshine@users.noreply.github.com>
2019-05-22 14:25:00 +0530

    Initial commit

---
+++ src/test/java/etap/etap/login/AuthorizationTest.java
@@ -0,0 +1,44 @@
+package etap.etap.login;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import com.cognifide.qa.bb.aem.core.api.AemActions;
+import com.cognifide.qa.bb.api.actions.ActionException;
+import com.cognifide.qa.bb.api.actions.ActionsController;
+import com.cognifide.qa.bb.junit5.guice.Modules;
+import com.cognifide.qa.bb.modules.BobcatRunModule;
+import com.cognifide.qa.bb.page.BobcatPageFactory;
+import com.google.inject.Inject;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+import etap.etap.pages.TestPage;
+
+/**
+ * Test for presenting how to log in to the page.
+ */
+@Modules(BobcatRunModule.class)
+@Epic("AEM 6.4 Base Tests")
+@Feature("Login to AEM")
+public class AuthorizationTest {
+
+	@Inject
+	private ActionsController controller;
+
+	@Inject
+	private BobcatPageFactory bobcatPageFactory;
+
+	@Test
+	@Story("Login to AEM and open Test page")
+	@Description("Login to author instance and open test page")
+	public void loginTest() throws ActionException {
+		TestPage testpage = bobcatPageFactory.create("/content/we-retail/us/en.html", TestPage.class);
+		controller.execute(AemActions.LOG_IN);
+		assertTrue(testpage.open().isDisplayed());
+	}
+
+}
